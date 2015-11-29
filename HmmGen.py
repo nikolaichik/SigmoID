05d1e1e2dde0fc206bd3961cd42bfd44f643f0c8
Aliaksandr Damienikan <fault.by@gmail.com>
2015-11-29 23:46:15 +0300

    correct version

--- HmmGen.py
+++ HmmGen.py
@@ -84,7 +84,7 @@ def createParser():
                         default=False,
                         help='''no duplicate features with the same location and the same protein_bind qualifier
                                 value''')
-    parser.add_argument('-v','--version', action='version', version='%(prog)s 2.11 (November 22, 2015)')
+    parser.add_argument('-v','--version', action='version', version='%(prog)s 2.12 (November 29, 2015)')
     parser.add_argument('-f', '--feature',
                         metavar='<"feature key">',
                         default='unknown type',
@@ -123,7 +123,7 @@ try:
     output_handle = open(enter.output_file, 'w')
 except IOError:
     sys.exit('Open error! Please check your genbank output path!')
-print '\nHmmGen 2.11 (November 22, 2015)'
+print '\nHmmGen 2.12 (November 29, 2015)'
 print "="*50
 print 'Options used:\n'
 for arg in range(1, len(sys.argv)):
