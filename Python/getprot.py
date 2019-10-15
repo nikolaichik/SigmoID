c4b83466c9057e91937678696912e326a6f1709c
Pavel Vychik <p.vychik@gmail.com>
2019-10-15 09:24:20 +0300

    getprot.py exception fix

--- Python/getprot.py
+++ Python/getprot.py
@@ -7,5 +7,16 @@ parser.add_argument('uniprotAcc')
 codes=parser.parse_args()
 
 Entrez.email = ""
-handle = Entrez.efetch(db="protein", id=codes.uniprotAcc, rettype="gb", retmode="text")
-print(handle.read())
+def fetch_data(codes):
+   handle = Entrez.efetch(db="protein", id=codes.uniprotAcc, rettype="gb", retmode="text")
+   data=handle.read()
+   if data.find("HTTP Error")>-1 and fail<=10:
+       fail=fail+1
+       print(fetch_data(codes))
+   else:
+       print(data)
+fetch_data(codes)
+    
+
+
+
