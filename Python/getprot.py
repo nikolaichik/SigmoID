6263c7d4736e8dc3f7b9b02b4a5628cd38dba017
Pavel Vychik <p.vychik@gmail.com>
2019-10-15 14:32:11 +0300

    getprot.py email identification added

--- Python/getprot.py
+++ Python/getprot.py
@@ -1,21 +1,24 @@
 # coding: utf-8
 from Bio import Entrez
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description='UniProt codes')
 parser.add_argument('uniprotAcc')
-codes=parser.parse_args()
-
-Entrez.email = ""
-def fetch_data(codes):
-   handle = Entrez.efetch(db="protein", id=codes.uniprotAcc, rettype="gb", retmode="text")
+parser.add_argument('email')
+args=parser.parse_args()
+Entrez.email = args.email
+def fetch_data(args):
+   handle = Entrez.efetch(db="protein", id=args.uniprotAcc, rettype="gb", retmode="text")
    data=handle.read()
-   if data.find("HTTP Error")>-1 and fail<=10:
-       fail=fail+1
-       print(fetch_data(codes))
+   handle.close()
+   if data.find("HTTP Error")>-1 and attemps<=3:
+       attemps=attemps+1
+       time.sleep(15)
+       print(fetch_data(args))
    else:
        print(data)
-fetch_data(codes)
+fetch_data(args)
     
 
 
