b5b8e227e0039ef42922164feb39d2818854baaf
Pavel Vychik <p.vychik@gmail.com>
2021-02-28 20:01:40 +0300

    CRtagExtractor.py improvements

--- Python/CRtagExtractor.py
+++ Python/CRtagExtractor.py
@@ -18,11 +18,11 @@ class InputFile:
             self.alignment = ''
         if name.endswith('.hmm'):
             with open(path, 'r') as fi:
-                for line in fi:
-                    if 'NAME' in line:
-                        self.hmmname = line.split(' ')[-1].replace('\n', '')
-                    if 'CRTAG' in line:
-                        self.crtag = line.split(' ')[-1].replace('\n', '')
+                for lin in fi:
+                    if 'NAME' in lin:
+                        self.hmmname = lin.split(' ')[-1].replace('\n', '')
+                    if 'CRTAG' in lin:
+                        self.crtag = lin.split(' ')[-1].replace('\n', '')
                         fi.close()
                         break
 
@@ -36,17 +36,17 @@ def mid(s, offset, amount):
     return s[offset:offset+amount]
 
 
-def extractcr(s, crtaglist):
-    CR = ''
+def extractTag(s, crtaglist):
+    residues = ''
     for position in crtaglist:
-        CR = CR + mid(s, int(position) - 1, 1)
-    return CR
+        residues = residues + mid(s, int(position) - 1, 1)
+    return residues
 
 
 def getCRtag(alignment):
     # comma in CRpositions marks two-steps CRtag processing
     combinedCRtag = ''
-    ProteinCode = ''
+    proteinCode = ''
     if ',' in CRpositions[0]:
         CRtagparts = CRpositions
     else:
@@ -54,83 +54,85 @@ def getCRtag(alignment):
 
     for part in CRtagparts:
         if part != '':
-            lCRpositions = part.split(',')
+            crResidues = part.split(',')
         else:
             return 'CRtag part processing resulted in error'
         if '/' not in alignment:
             return 'error alignment input'
-        ProteinCode = alignment.split('/')[0]
-        HitSeq = alignment.split()[1]  # get amino acid sequence
-        HitSeq = HitSeq.replace('.', '')  # remove gaps from insertions in other seqs
+        proteinCode = alignment.split('/')[0]
+        # get amino acid sequence and remove gaps from insertions in other seqs
+        hitSeq = alignment.split()[1]
+        hitSeq = hitSeq.replace('.', '')
         # now extend hits to equal length at ends
-        if '-' in HitSeq:
-            cuttedHitSeq = HitSeq
+        if '-' in hitSeq:
+            cuttedhitSeq = hitSeq
             index = 0
-            if cuttedHitSeq[0] == '-':
-                for index in range(len(cuttedHitSeq)):
-                    if cuttedHitSeq[index] != '-':
+            if cuttedhitSeq[0] == '-':
+                for index in range(len(cuttedhitSeq)):
+                    if cuttedhitSeq[index] != '-':
                         break
-                cuttedHitSeq = cuttedHitSeq[index:]
-            if len(cuttedHitSeq) != 0:
-                while cuttedHitSeq[-1] == '-':
-                    cuttedHitSeq = cuttedHitSeq[0:-1]  # iterate string right to left and cutoff '-'
-            p = subprocess.Popen(blastdbcmd+' -db {} -entry {}'.format(str(basePath), quote(ProteinCode)),
+                cuttedhitSeq = cuttedhitSeq[index:]
+            # iterate string right to left and cutoff '-'
+            if len(cuttedhitSeq) != 0:
+                while cuttedhitSeq[-1] == '-':
+                    cuttedhitSeq = cuttedhitSeq[0:-1]
+            p = subprocess.Popen(blastdbcmd+' -db {} -entry {}'.format(str(basePath), quote(proteinCode)),
                                  stdout=subprocess.PIPE, shell=True)
             out, error = p.communicate()
             if p.returncode != 0:
                 print('\nfull protein sequence extraction with blastdbcmd failed, empty protein sequence returned.' + \
                 'Return code: ' + str(p.returncode))
-            ProteinCompleteSeq = out.decode('Utf-8').split('\n')
-            if len(ProteinCompleteSeq) == 0 or ProteinCompleteSeq == '':
-                print('\nfull protein sequence extraction with blastdbcmd resulted in empty protein sequence returned.' + \
-                      '\nCommand used: ' + str(blastdbcmd+' -db {} -entry {}'.format(str(basePath), quote(ProteinCode))))
+            proteinFullSeq = out.decode('Utf-8').split('\n')
+            if len(proteinFullSeq) == 0 or proteinFullSeq == '':
+                print('\nfull protein sequence extraction with blastdbcmd resulted in empty protein sequence returned.' +
+                      '\nCommand used: ' + str(blastdbcmd+' -db {} -entry {}'.format(str(basePath), quote(proteinCode))))
             # drop first line with description and concatenate sequence containing lines in string
-            ProteinCompleteSeq = ''.join(ProteinCompleteSeq[1:])
-            if ProteinCompleteSeq == '':
-                return 'empty seq for:' + ProteinCode
-            gapindex = cuttedHitSeq.find('-')
+            proteinFullSeq = (''.join(proteinFullSeq[1:])).upper()
+            if proteinFullSeq == '':
+                return 'empty seq for:' + proteinCode
+            gapindex = cuttedhitSeq.find('-')
             if gapindex != -1:
-                leftPart = cuttedHitSeq[:gapindex]
+                leftPart = cuttedhitSeq[:gapindex]
             else:
-                leftPart = cuttedHitSeq
+                leftPart = cuttedhitSeq
             try:
-                leftPartStart = ProteinCompleteSeq.find(leftPart.upper())
+                leftPartStart = proteinFullSeq.find(leftPart.upper())
             except AttributeError:
-                print('Protein code error: "' + str(ProteinCode) + '"')
-                return 'short tag ' + ProteinCode
-            leftExt = mid(ProteinCompleteSeq, leftPartStart-index, index)
-            cuttedHitSeq = leftExt + cuttedHitSeq
+                print('Protein code error: "' + str(proteinCode) + '"')
+                return 'short tag ' + proteinCode
+            leftExt = mid(proteinFullSeq, leftPartStart-index, index)
+            cuttedhitSeq = leftExt + cuttedhitSeq
 
             # extend the right end
-            rightPart = cuttedHitSeq.split('-')[-1]
-            rightPartStart = ProteinCompleteSeq.find(rightPart.upper())
-            rightExt = mid(ProteinCompleteSeq, rightPartStart + len(rightPart), len(HitSeq) - len(cuttedHitSeq))
-            HitSeq = cuttedHitSeq + rightExt
+            rightPart = cuttedhitSeq.split('-')[0]
+            rightPartStart = proteinFullSeq.find(rightPart.upper())
+            rightExt = mid(proteinFullSeq, rightPartStart + len(rightPart), len(hitSeq) - len(cuttedhitSeq))
+            hitSeq = cuttedhitSeq + rightExt
 
         # gaps in the modelbefore the CRtag region should be corrected
-        CRtagRegion = HitSeq[:int(lCRpositions[0])]
+        CRtagRegion = hitSeq[:int(crResidues[0])]
         if CRtagRegion != CRtagRegion.upper():
             gapChars = 0
             for char in CRtagRegion:
                 if char.islower():
                     gapChars += 1
 
-            HitSeq = HitSeq[-(len(HitSeq)-gapChars):]
+            hitSeq = hitSeq[-(len(hitSeq)-gapChars):]
 
         # checking for gaps/insertions within CR range
-        CRtagRegion = HitSeq[int(lCRpositions[0])-1:int(lCRpositions[-1])]
+        CRtagRegion = hitSeq[int(crResidues[0])-1:int(crResidues[-1])]
         if CRtagRegion == CRtagRegion.upper() and '-' not in CRtagRegion:
-            CRtag = extractcr(HitSeq, lCRpositions)
+            CRtag = extractTag(hitSeq, crResidues)
         else:
-            return 'indel within CR tag region ' + ProteinCode
-        if len(CRtag) == len(lCRpositions):
+            return 'indel within CR tag region ' + proteinCode
+        if len(CRtag) == len(crResidues):
             if len(CRtagparts) == 1:
-                return CRtag+' ' + ProteinCode
+                return CRtag+' ' + proteinCode
             else:
                 combinedCRtag = combinedCRtag+CRtag
         else:
-            return 'short tag ' + ProteinCode
-    return combinedCRtag + ' ' + ProteinCode
+            return 'short tag ' + proteinCode
+    return combinedCRtag + ' ' + proteinCode
 
 
 def checkDB(pathseq, fileslocation):
@@ -146,13 +148,18 @@ def checkDB(pathseq, fileslocation):
     return True
 
 
-parser = argparse.ArgumentParser(description='Extract CRtag from protein sequence provided by hmmsearch ')
+parser = argparse.ArgumentParser(prog='CRtagExtractor',
+                                usage='\n%(prog)s <folder with protein seqs fasta> <.hmm models folder> <temp folder> <output folder> '
+                                '<number of cpu workers> <ncbi-blast tools folder>',
+                                 epilog='Pavel Vychyk, 2019',
+                                 description='Extract CRtag from protein sequence provided by hmmsearch ')
 parser.add_argument('fastaSource', type=str, help='path to folder with fasta files')
 parser.add_argument('hmmSource', type=str, help='path to folder with .hmm models ')
 parser.add_argument('alignmentFolder', type=str, help='path to output folder for alignments')
 parser.add_argument('baseFolder', type=str, help='path to folder to save ready CRtag bases')
 parser.add_argument('numberOfWorkers', type=str, help='number of cpu workers for hmmsearch and CRtag extraction')
 parser.add_argument('blastFolder', type=str, help='path to the folder with installed ncbi-blast utilities: makeblastdb, blastdbcmd')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.01 (February 28, 2021)')
 args = parser.parse_args()
 
 
@@ -174,14 +181,14 @@ if alignmentFolder[-1] != '/':
 # read input folder, process .fasta files with hmmsearch and cdbfasta to get alignments with all .hmm models provided by separate input folder
 # after that, process alignment files to get crtags bases
 
-InputFasta = []
+inputFasta = []
 with os.scandir(fastaSource) as FilesEntity:
     for fil in FilesEntity:
         if fil.is_file and fil.name.endswith('.fasta'):
             fasta = InputFile(fil.path, fil.name)
-            InputFasta.append(fasta)
-if len(InputFasta) == 0:
-    print('No .fasta found in folder: '+str(fastaSource) + ' File names must have .fasta extension!'+'\n')
+            inputFasta.append(fasta)
+if len(inputFasta) == 0:
+    print('No .fasta files were found in folder: '+str(fastaSource) + ' Source file must have .fasta extension!'+'\n')
     sys.exit(1)
 hmmFiles = []
 with os.scandir(hmmSource) as FilesEntity:
@@ -210,16 +217,16 @@ with os.scandir(blastUtilities) as FilesEntity:
                 print('\nblastdbcmd test run resulted in error: ' + str(p.returncode))
                 sys.exit(1)
 if len(hmmFiles) == 0:
-    print('No .hmm found in folder: '+str(fastaSource) + ' File names must have .hmm extension!'+'\n')
+    print('No .hmm files were found in folder: '+str(fastaSource) + ' File must have .hmm extension!'+'\n')
     sys.exit(1)
 err = ['indel', 'short', 'error', 'empty']
-for fasta in InputFasta:
+for fasta in inputFasta:
     if os.path.exists(fasta.path):
         basePath = fasta.path.replace('.fasta', '_blastDB')
         if os.path.exists(basePath+'.pal'):
             with open(basePath+'.pal', 'r') as blastDBinfo:
-                for line in blastDBinfo:
-                    if 'DBLIST' in line and not checkDB(line, fasta.path):
+                for lin in blastDBinfo:
+                    if 'DBLIST' in lin and not checkDB(lin, fasta.path):
                         p = subprocess.Popen(makeblastdb+' -dbtype prot -in {} -parse_seqids -hash_index -out {}'.format(str(fasta.path),
                                              str(basePath)), stdout=PIPE, stderr=PIPE, shell=True)
                         out, error = p.communicate()
@@ -248,87 +255,86 @@ for fasta in InputFasta:
                     reg = re.search('(?<=rp)\d\d', fasta.name)
                     if reg is not None:
                         clust = reg.group(0)
-                        BaseFilePath = baseFolder + str(hmmFile.name.split('.')[0]) + '_rp'+str(clust) + '.crtag'
+                        baseFilePath = baseFolder + str(hmmFile.name.split('.')[0]) + '_rp'+str(clust) + '.crtag'
                     else:
                         # write index to file name, if path exists
-                        BaseFilePath = baseFolder+str(hmmFile.name.split('.')[0])+'.crtag'
-                        if os.path.exists(BaseFilePath):
+                        baseFilePath = baseFolder+str(hmmFile.name.split('.')[0])+'.crtag'
+                        if os.path.exists(baseFilePath):
                             i = 0
                             while os.path.exists(baseFolder + str(hmmFile.name.split('.')[0]) + '-'+str(i) + '.crtag'):
                                 i += 1
-                            BaseFilePath = baseFolder + str(hmmFile.name.split('.')[0]) + '-' + str(i) + '.crtag'
+                            baseFilePath = baseFolder + str(hmmFile.name.split('.')[0]) + '-' + str(i) + '.crtag'
 
                     # read input file line by line, save protein ids separately - proteins with multiple domain should be removed
-                    ProteinIDs = []
+                    proteinIDs = []
                     with open(fasta.alignment) as fin:
-                        AlignmentTable = ''
-                        for line in fin:
-                            if line[:1].isalpha() or line[:1].isdigit():
-                                AlignmentTable += line
-                                ProteinIDs.append(line.split('/')[0])
-                    AlignmentTable = AlignmentTable.split('\n')[:-1]
+                        alignmentTable = ''
+                        for lin in fin:
+                            if lin[:1].isalpha() or lin[:1].isdigit():
+                                alignmentTable += lin
+                                proteinIDs.append(lin.split('/')[0])
+                    alignmentTable = alignmentTable.split('\n')[:-1]
 
                     uniq = {}
                     multidomain = []
-
-                    for id in ProteinIDs:
-                        if id not in uniq:
-                            uniq[id] = 1
+                    for idn in proteinIDs:
+                        if idn not in uniq:
+                            uniq[idn] = 1
                         else:
-                            multidomain.append(id)
+                            multidomain.append(idn)
 
-                    for AlignmentSeq in reversed(AlignmentTable):
-                        ProteinID = AlignmentSeq.split('/')[0]
-                        if ProteinID in multidomain:
-                            AlignmentTable.remove(AlignmentSeq)
+                    for AlignmentSeq in reversed(alignmentTable):
+                        proteinID = AlignmentSeq.split('/')[0]
+                        if proteinID in multidomain:
+                            alignmentTable.remove(AlignmentSeq)
 
                     CRtagProt = []
 
                     if __name__ == '__main__':
                         with Pool(int(cpuCount)) as p:
-                            CRtagProt = p.map(getCRtag, AlignmentTable)
+                            CRtagProt = p.map(getCRtag, alignmentTable)
 
                     base = {}
                     indel = 0
                     shortTag = 0
                     CRtagCount = 0
-                    brokenTagId = ''
+                    brokenTagID = ''
                     if len(CRtagProt) > 0:
                         for seq in CRtagProt:
                             if not any(c in seq for c in err):
                                 CRtagCount += 1
-                                CRtag, ProteinID = seq.split(' ')
+                                CRtag, proteinID = seq.split(' ')
                                 if CRtag not in base:
-                                    base[CRtag] = ProteinID
+                                    base[CRtag] = proteinID
                                 else:
-                                    base[CRtag] = base[CRtag]+','+ProteinID
+                                    base[CRtag] = base[CRtag]+','+proteinID
                             else:
                                 if 'indel' in seq:
                                     indel += 1
                                 elif 'short' in seq:
                                     shortTag += 1
-                                    brokenTagId = brokenTagId + seq.split()[2] + '\n'
+                                    brokenTagID = brokenTagID + seq.split()[2] + '\n'
                                 elif 'empty' in seq:
                                     print(str(seq)+'\n')
                     else:
                         print('CRtag array is empty')
 
                     try:
-                        with open(BaseFilePath, "w") as f:
+                        with open(baseFilePath, "w") as f:
                             data = ''
                             for key, value in base.items():
                                 data += key + ' ' + value + '\n'
                             f.write(data)
                             f.close()
-                        if brokenTagId != '':
-                            BaseFilePath = BaseFilePath + '_short_tags'
-                            with open(BaseFilePath, "w") as f:
-                                f.write(brokenTagId)
+                        if brokenTagID != '':
+                            baseFilePath = baseFilePath + '_short_tags'
+                            with open(baseFilePath, "w") as f:
+                                f.write(brokenTagID)
                                 f.close()
                     except IOError:
-                        print("Can't save file to disk, check the path: "+BaseFilePath)
-                    print('\nModel used: ' + str(hmmFile.name) + '\nTotal proteins processed: ' + str(len(AlignmentTable)) +
+                        print("Can't save file to disk, check the path: "+baseFilePath)
+                    print('\nModel used: ' + str(hmmFile.name) + '\nTotal proteins processed: ' + str(len(alignmentTable)) +
                           '\nCRtags extracted: ' + str(CRtagCount) + '\nProteins with indel in CRtag region: ' + str(indel) +
-                          "\nBroken tags (CRtag length isn't equal to the quantity of critical residues): " + str(shortTag))
+                          "\nBroken tags (CRtag length doesn't equal to the quantity of critical residues): " + str(shortTag))
                     if shortTag != 0:
-                        print('\nFull list of protein IDs with broken tags located: ' + str(BaseFilePath) + '\n')
+                        print('\nFull list of protein IDs with broken tags located: ' + str(baseFilePath) + '\n')
